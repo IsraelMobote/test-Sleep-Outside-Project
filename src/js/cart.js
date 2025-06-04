24c7b1d9499867448389008a3e25d2befe53dc32
IsraelMobote <moboteisrael02@gmail.com>
2025-06-04 07:39:30 +0100

    edit

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,16 +1,55 @@
 import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
+const cartItems = getLocalStorage("so-cart");
+
+let total;
+const totalSpanElement = document.querySelector(".totalPrice");
+
+
+returnTotal();
+
+displayTotalPrice();
+
+renderCartContents();
+
+loadHeaderFooter();
+
+function updateCheckoutLink() {
+  const checkoutLink = document.querySelector(".checkout");
+  checkoutLink.href = `../checkout/?total=${total}`;
+
+}
+
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  console.log(cartItems);
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+}
+
+async function displayTotalPrice() {
+
+  total = await returnTotal();
+
+  totalSpanElement.textContent = total;
+  updateCheckoutLink();
+
+}
+
+function returnTotal() {
+  let total = 0;
+  cartItems.forEach(item => {
+    const itemtotal = item.FinalPrice * getLocalStorage(item.Name);
+    total += itemtotal;
+  });
+
+  return total;
 }
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimaryMedium}"
       alt="${item.Name}"
     />
   </a>
@@ -18,13 +57,9 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">${getLocalStorage(item.Name)}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
   return newItem;
 }
-
-renderCartContents();
-
-loadHeaderFooter();
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,31 @@
+import CheckoutProcess from "./checkoutProcess.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+const checkoutProcess = new CheckoutProcess("so-cart", ".order-summary");
+
+const formInputs = document.querySelectorAll('form input');
+
+formInputs[5].addEventListener("input", () => {
+    if(checkFormZipCodeIsFilled()) {
+        checkoutProcess.init();
+    }
+})
+
+let checkFormZipCodeIsFilled = function() {
+
+       let formZipCodeFilled = false;
+
+        if (formInputs[5].validity.valid) {
+
+            formZipCodeFilled = true;
+        }
+
+
+    return formZipCodeFilled;
+}
+
+
+
+checkoutProcess.calculateITemSubTotal();
